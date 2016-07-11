edac06f337692c59f18cbbfa86356b0b36f4fbe0
Brian Cohen <brian.cohen.88@gmail.com>
2016-07-11 00:38:39 -0400

    minor simplification/migration fix

--- control.lua
+++ control.lua
@@ -27,22 +27,21 @@ script.on_event(defines.events.on_player_placed_equipment, function(event)
   end
 end)
 
+function minusnat(n, m)
+  if m >= n
+    then return 0
+    else return n - m
+  end
+end
+
 script.on_event(defines.events.on_player_removed_equipment, function(event)
   local player = game.players[event.player_index]
   if event.equipment == "grabber-equipment"
     then
-      if player.character_reach_distance_bonus >= 4 * event.count
-        then
-          player.character_reach_distance_bonus = player.character_reach_distance_bonus - 4 * event.count
-          player.character_build_distance_bonus = player.character_build_distance_bonus - 4 * event.count
-          player.character_item_drop_distance_bonus = player.character_item_drop_distance_bonus - 4 * event.count
-          player.character_resource_reach_distance_bonus = player.character_resource_reach_distance_bonus - 2 * event.count
-        else
-          player.character_reach_distance_bonus = 0
-          player.character_build_distance_bonus = 0
-          player.character_item_drop_distance_bonus = 0
-          player.character_resource_reach_distance_bonus = 0
-      end
+      player.character_reach_distance_bonus = minusnat(player.character_reach_distance_bonus, 4 * event.count)
+      player.character_build_distance_bonus = minusnat(player.character_build_distance_bonus, 4 * event.count)
+      player.character_item_drop_distance_bonus = minusnat(player.character_item_drop_distance_bonus, 4 * event.count)
+      player.character_resource_reach_distance_bonus = minusnat(player.character_resource_reach_distance_bonus, 2 * event.count)
       if debug
         then
           player.print(player.name)
